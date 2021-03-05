f2ea026165b51629ea81e5f502f7fc97886b58ad
mk579 <2258373673@qq.com>
2021-03-05 09:07:10 +0800

    GitHub的第一个案例

--- Test/src/chaoyanglove/com/index.java
+++ Test/src/chaoyanglove/com/index.java
@@ -2,10 +2,6 @@ package chaoyanglove.com;
 
 public class index {
     public static void main(String[] args) {
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
+        System.out.println("hello git");
     }
 }
